5f209d88cbdc513914a30da4e93e48d90cda94ed
Pavel Shvetsov <shvedusa@gmail.com>
2021-05-25 19:23:40 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,9 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        //created new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and .iml file
+        //shared project on github
     }
 }
